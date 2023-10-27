140340d557491000aa61fc27f32f47c2b5d01572
madisec <madiisec@gmail.com>
2023-10-28 02:07:37 +0330

    Add shuffledns binary file

--- scripts/dbrute.sh
+++ scripts/dbrute.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo $1 | skubfinder -silent | rev | cut -d "." -f3 | rev | sort -u > ~/.subcon/dw
+echo $1 | ~/.subcon/bin/subfinder -silent | rev | cut -d "." -f3 | rev | sort -u > ~/.subcon/dw
 shuffledns -l ~/.subcon/dw -r ~/.subcon/resolver -silent 
